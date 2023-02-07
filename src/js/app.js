d697e60834414934964cd13fbdda2894c666c974
elmiramm <elmira.m.m@yandex.ru>
2023-02-07 09:53:54 +0300

    burger - toggle - close btn

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,24 @@ const swiper = new Swiper('.swiper', {
 	// configure Swiper to use modules
 	modules: [Navigation, Pagination],
 });
+
+window.onload = function () {
+	document.addEventListener('click', documentActions);
+
+	//------Действия-при-кликах------
+	function documentActions(e) {
+		const targetElement = e.target;
+
+
+		// Если кликнули на бургер
+		if (targetElement.classList.contains('burger') || targetElement.closest('.burger')) {
+
+			const elem = targetElement.classList.contains('burger') ? targetElement : targetElement.closest('.burger');
+			elem.classList.toggle('active-burger');
+
+			const menuBody = document.querySelector('.menu');
+			menuBody.classList.toggle('menu-active');
+		}
+	}
+	//------Действия-при-кликах-End-----
+}
