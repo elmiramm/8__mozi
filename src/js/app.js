74f4d34d4c2366ba56cbd5d316cfc89e2b5c5e2f
elmiramm <elmira.m.m@yandex.ru>
2023-02-16 21:03:46 +0300

    innovation - html, swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,43 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-	// configure Swiper to use modules
-	modules: [Navigation, Pagination],
-});
+import Swiper, { Navigation, Pagination, Parallax } from 'swiper';
+// ------Swiper-------
+Swiper.use([Navigation, Pagination, Parallax]);
+
+if (document.querySelector('.slider-innovation__body')) {
+	new Swiper('.slider-innovation__body', {
+		observer: true,
+		observeParents: true,
+		slidesPerView: 4,
+		spaceBetween: 30,
+		// watchOverflow: true,
+		speed: 800,
+		loop: true,
+		// loopAdditionalSlides: 8,
+		preloadImage: false,
+		// parallax: true,
+		navigation: {
+			nextEl: '.slider-innovation .slider-arrow--next',
+			prevEl: '.slider-innovation .slider-arrow--prev',
+		},
+		// breakpoints: {
+		// 	279: {
+		// 		slidesPerView: 1.1,
+		// 		spaceBetween: 15,
+		// 	},
+		// 	768: {
+		// 		slidesPerView: 2,
+		// 		spaceBetween: 20,
+		// 	},
+		// 	992: {
+		// 		slidesPerView: 4,
+		// 		spaceBetween: 32,
+		// 	},
+		// }
+	});
+}
+// ------Swiper-End-------
 
 window.onload = function () {
 	document.addEventListener('click', documentActions);
